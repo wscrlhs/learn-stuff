9912b2136285ed3e25b337eab339e6f7e4becad6
wscrlhs <wscrlhs@gmail.com>
2018-01-12 14:53:43 +0800

    js获取cookie

---
+++ cookie.js
@@ -0,0 +1,9 @@
+//js获取cookie的方法
+function getCookie(name)
+    {
+        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+        if(arr=document.cookie.match(reg))
+            return unescape(arr[2]);
+        else
+            return null;
+    }
